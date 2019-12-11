f121812254cf861182ab89e1c4745f7882d561b9
Anıl Aydın <anil_aydinn@outlook.com>
2019-12-12 00:15:52 +0300

    LoginPage Brute Force control added.

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -17,15 +17,16 @@ import java.awt.Font;
 import java.awt.Image;
 
 import javax.swing.JPasswordField;
+import java.util.Random;
 
 import java.awt.Image;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JCheckBox;
 public class LoginPage extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField txtNj;
-	private JTextField textField_2;
 	private JPasswordField passwordField;
 	private ProductOperationsDB productOperationsDB = new ProductOperationsDB();
 
@@ -59,8 +60,12 @@ public class LoginPage extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		setLocationRelativeTo(null);
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		JCheckBox chckbxBruteForce = new JCheckBox("Are you human ?");
+		chckbxBruteForce.setBounds(75, 377, 146, 23);
+		contentPane.add(chckbxBruteForce);
 		
 		JButton btnLogin = new JButton("SignIn");
 		btnLogin.addActionListener(new ActionListener() {
@@ -70,22 +75,27 @@ public class LoginPage extends JFrame {
 				String password = new String(passwordField.getPassword());
 				boolean loginSuccess = productOperationsDB.login(username, password);
 				
-				if(loginSuccess) {
+				
+				if(loginSuccess && chckbxBruteForce.isSelected()) {
 					
 					Inside inside = new Inside();
 					inside.setVisible(true);
 					setVisible(false);
 				}
-				else {
+				else if(chckbxBruteForce.isSelected() != true) {
 					
+					JOptionPane.showMessageDialog(null, "Please prove you are human!");
+				}
+				else {
+				
 					JOptionPane.showMessageDialog(null, "Username or Password incorrect.");
 				}
 							
-							}
+			}
 		});
 		btnLogin.setFont(new Font("Rockwell", Font.BOLD, 18));
 		btnLogin.setBackground(new Color(255, 255, 255));
-		btnLogin.setBounds(280, 429, 110, 47);
+		btnLogin.setBounds(201, 429, 110, 47);
 		contentPane.add(btnLogin);
 		
 		txtNj = new JTextField();
@@ -94,15 +104,10 @@ public class LoginPage extends JFrame {
 		contentPane.add(txtNj);
 		txtNj.setColumns(10);
 		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(271, 369, 46, 22);
-		contentPane.add(textField_2);
-		
 		JLabel lblNewLabel = new JLabel("Username: ");
 		lblNewLabel.setFont(new Font("Rockwell", Font.BOLD, 18));
 		lblNewLabel.setForeground(new Color(0, 0, 0));
-		lblNewLabel.setBounds(75, 274, 157, 16);
+		lblNewLabel.setBounds(75, 274, 128, 16);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblPassword = new JLabel("Password: ");
@@ -114,22 +119,12 @@ public class LoginPage extends JFrame {
 		passwordField.setBounds(201, 320, 116, 22);
 		contentPane.add(passwordField);
 		
-		JLabel lblBruteforceControl = new JLabel("Brute-Force Control: ");
-		lblBruteforceControl.setFont(new Font("Rockwell", Font.BOLD, 18));
-		lblBruteforceControl.setBounds(74, 356, 262, 47);
-		contentPane.add(lblBruteforceControl);
-		
 		JLabel lblSunIcon = new JLabel("");
 		Image img = new ImageIcon(this.getClass().getResource("/Cafe.png")).getImage();
 		lblSunIcon.setIcon(new ImageIcon(img)); 
 		lblSunIcon.setBounds(491, -31, 300, 373);
 		contentPane.add(lblSunIcon);
-		
-		JLabel labelCheck = new JLabel("");
 		Image img1 = new ImageIcon(this.getClass().getResource("/tikk.png")).getImage();
-		labelCheck.setIcon(new ImageIcon(img1)); 
-		labelCheck.setBounds(329, 356, 61, 47);
-		contentPane.add(labelCheck);
 		
 		JButton btnSignup = new JButton("SignUp");
 		btnSignup.addActionListener(new ActionListener() {
@@ -142,7 +137,7 @@ public class LoginPage extends JFrame {
 		});
 		btnSignup.setFont(new Font("Rockwell", Font.BOLD, 18));
 		btnSignup.setBackground(Color.WHITE);
-		btnSignup.setBounds(409, 429, 110, 47);
+		btnSignup.setBounds(336, 429, 110, 47);
 		contentPane.add(btnSignup);
 		
 	}
